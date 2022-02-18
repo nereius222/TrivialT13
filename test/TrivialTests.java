457590375c05e131c635d3526abcb294e1586f49
Nerea Pérez Pizarro <nerea.perezpizarro@iesfm.onmicrosoft.com>
2022-02-18 10:19:42 +0100

    Eliminamos código de posición de jugador

--- test/TrivialTests.java
+++ test/TrivialTests.java
@@ -12,4 +12,19 @@ public class TrivialTests {
     public void crear_Game(){
         Game trivial = new Game();
     }
+
+    @Test
+    public void si_al_principio_saco_un_1_voy_a_casilla_1() {
+        Game sut = new Game();
+        sut.agregar("María");
+        sut.agregar("Juan");
+
+        sut.tirarDado(1);
+
+        String expected = "La nueva posición de María es 1";
+
+        String actual = sut.nuevaPosicionJugador();
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
